1b9fb56b9369ecbc347edcbdd6affd274901d9d7
shaileshSwabhav <shailesh@swabhavtechlabs.com>
2022-04-07 11:27:00 +0530

    first commit

---
+++ config/Constants.go
@@ -0,0 +1,15 @@
+package config
+
+// Env constants
+const (
+	DBRootPassword   = "DB_ROOT_PASSWORD"
+	DBUser           = "DB_USER"
+	DBPassword       = "DB_PASSWORD"
+	DBDatabase       = "DB_DATABASE"
+	DBPort           = "DB_PORT"
+	DBHost           = "DB_HOST"
+	AccessSecretKey  = "ACCESS_SECRET"
+	HTTPWriteTimeout = "HTTP_WRITE_TIMEOUT"
+	HTTPReadTimeout  = "HTTP_READ_TIMEOUT"
+	HTTPIdleTimeout  = "HTTP_IDLE_TIMEOUT"
+)
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/shaileshSwabhav/tsam
+
+go 1.16
+
+require (
+	github.com/gookit/color v1.5.0
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/viper v1.10.1
+)
